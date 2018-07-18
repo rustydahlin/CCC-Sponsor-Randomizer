203a5cb73aa26ba049c289e4f1175512f0f09619
rustydahlin <rustydahlin@hotmail.com>
2018-07-18 16:22:25 -0500

    -

--- image-randomizer.py
+++ image-randomizer.py
@@ -2,9 +2,9 @@ import os, random
 import shutil
 
 # Randomly grab 1 image, and copy/rename to static directory as image1.png
-randomimage1 = random.choice(os.listdir("Images-Sponsors"))
-shutil.copy2("Images-Sponsors/" + randomimage1, "Images-Static/image1.png")
+randomimage1 = random.choice(os.listdir("Images-Storage"))
+shutil.copy2("Images-Storage/" + randomimage1, "Images-Static/image1.png")
 
 # Randomly grab 1 image, and copy.rename to static direcotry as image2.png
-randomimage2 = random.choice(os.listdir("Images-Sponsors"))
-shutil.copy2("Images-Sponsors/" + randomimage2, "Images-Static/image2.png")
+randomimage2 = random.choice(os.listdir("Images-Storage"))
+shutil.copy2("Images-Storage/" + randomimage2, "Images-Static/image2.png")
